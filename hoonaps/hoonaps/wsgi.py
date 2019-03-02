afaffccdb1b98a2e126157d72180293cf0191eec
Michael Tang <mchl.tang@gmail.com>
2019-03-02 12:38:26 -0500

    changed project name

--- hoonaps/hoonaps/wsgi.py
+++ hoonaps/hoonaps/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hoohacks19 project.
+WSGI config for hoonaps project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hoohacks19.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hoonaps.settings')
 
 application = get_wsgi_application()
